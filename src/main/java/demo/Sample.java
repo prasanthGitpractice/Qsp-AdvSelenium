b05960bad58d1093ef870e43a19c246af2e8995e
prasanthGitpractice <galiprasanth6@gmail.com>
2023-09-12 15:36:03 +0530

    Commit and Push

---
+++ src/main/java/demo/Sample.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This is my First commit to Github");
+
+	}
+
+}
