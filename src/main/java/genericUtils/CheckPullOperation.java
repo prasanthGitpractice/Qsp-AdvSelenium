8e793d0a477834ce4762b8e326d71b092b2005de
prasanthGitpractice <galiprasanth6@gmail.com>
2023-09-13 15:00:32 +0530

    check pull Operation

---
+++ src/main/java/genericUtils/CheckPullOperation.java
@@ -0,0 +1,10 @@
+package genericUtils;
+
+public class CheckPullOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Changes were pulled successfully");
+	}
+
+}
